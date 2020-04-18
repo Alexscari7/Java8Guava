8e4e25c7dfd69fb3445822c7584eacbadbc5c6e3
alexscari <alexscar777@gmail.com>
2020-04-18 11:32:09 +0800

    添加test

---
+++ src/test/java/test/FirstStart.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author wusd
+ * @description 空
+ * @create 2020/04/18 11:27
+ */
+public class FirstStart {
+    @Test
+    public void test1() {
+        System.out.println(1);
+    }
+
+}
