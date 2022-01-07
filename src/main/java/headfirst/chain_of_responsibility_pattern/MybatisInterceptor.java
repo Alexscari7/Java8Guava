9be4bafa8d5d6d77639dc0f6d96eefcd7afbfd48
wushengdong <alexscar777@gmail.com>
2022-01-07 09:34:22 +0800

    add: 增加Interceptor 3

--- src/main/java/headfirst/chain_of_responsibility_pattern/MybatisInterceptor.java
+++ src/main/java/headfirst/chain_of_responsibility_pattern/MybatisInterceptor.java
@@ -2,14 +2,21 @@ package headfirst.chain_of_responsibility_pattern;
 
 import com.google.common.collect.Lists;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author wusd
@@ -20,11 +27,16 @@ public class MybatisInterceptor {
         Map<String, String> map = new HashMap<>();
         map.put("1", "1");
         List<String> list = Lists.newArrayList("1");
-        Interceptor mapInterceptor = new MapAlwaysNullInterceptor();
-        Interceptor listInterceptor = new ListAlwaysEmptyInterceptor();
-        InterceptorChain interceptorChain = new InterceptorChain().addInterceptor(mapInterceptor).addInterceptor(listInterceptor);
+
+        Interceptor mapInterceptor = new AlwaysNullInterceptor();
+        Interceptor listInterceptor = new AlwaysEmptyInterceptor();
+        InterceptorChain interceptorChain = new InterceptorChain()
+                .addInterceptor(mapInterceptor)
+                .addInterceptor(listInterceptor);
+
         List o = (List)interceptorChain.applyTo(list);
         System.out.println(o.get(1));
+        System.out.println(o.size());
     }
 
 
@@ -45,22 +57,32 @@ class Plugin implements InvocationHandler {
     }
 
     public static Object apply(Object target, Interceptor interceptor) throws NoSuchMethodException {
-        Signature sig = interceptor.getClass().getAnnotation(Signature.class);
-        Class<?> type = sig.type();
-        Method method = type.getMethod(sig.method(), sig.args());
-        if (type.getInterfaces().length > 0) {
+        // 确认目标对象是否适配Interceptor中指定代理类。适配时代理，不适配时直接使用原对象
+        Intercept sig = interceptor.getClass().getAnnotation(Intercept.class);
+        Method proxyMethod = sig.type().getMethod(sig.method(), sig.args());
+        List<Class<?>> interfaces = getAllInterfaces(target.getClass());
+        if (interfaces.contains(sig.type())) {
             return Proxy.newProxyInstance(
                     target.getClass().getClassLoader(),
-                    target.getClass().getInterfaces(),
-                    new Plugin(target, interceptor, method)
+                    interfaces.toArray(new Class[0]),
+                    new Plugin(target, interceptor, proxyMethod)
             );
         }
         return target;
     }
 
+    private static List<Class<?>> getAllInterfaces(Class<?> type) {
+        Set<Class<?>> interfaces = new HashSet<>();
+        for (; type != null; type = type.getSuperclass()) {
+            interfaces.addAll(Arrays.asList(type.getInterfaces()));
+        }
+        return new ArrayList<>(interfaces);
+    }
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (method == this.method) {
+        // 确认调用方法是否为指定代理方法。匹配时代理方法，不匹配时执行原方法
+        if (method.equals(this.method)) {
             return interceptor.intercept(target, method, args);
         }
         return method.invoke(target, args);
@@ -72,14 +94,17 @@ interface Interceptor {
     Object intercept(Object target, Method method, Object[] args) throws InvocationTargetException, IllegalAccessException;
 }
 
-@interface Signature{
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@interface Intercept {
     Class<?> type();
     String method();
     Class<?>[] args();
 }
 
 class InterceptorChain {
-    private List<Interceptor> interceptors = new ArrayList<>();
+
+    private final List<Interceptor> interceptors = new ArrayList<>();
 
     public Object applyTo(Object target) throws NoSuchMethodException {
         for (Interceptor interceptor : interceptors) {
@@ -95,16 +120,16 @@ class InterceptorChain {
 
 }
 
-@Signature(type=Map.class, method="get", args={Object.class})
-class MapAlwaysNullInterceptor implements Interceptor {
+@Intercept(type=Map.class, method="get", args={Object.class})
+class AlwaysNullInterceptor implements Interceptor {
     @Override
     public Object intercept(Object target, Method method, Object[] args) {
         return "null";
     }
 }
 
-@Signature(type=List.class, method="get", args={Integer.class})
-class ListAlwaysEmptyInterceptor implements Interceptor {
+@Intercept(type=List.class, method="get", args={int.class})
+class AlwaysEmptyInterceptor implements Interceptor {
     @Override
     public Object intercept(Object target, Method method, Object[] args) {
         return "empty";
